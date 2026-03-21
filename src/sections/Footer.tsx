c97bc15a850e03f11803bde67bf48b18924ab5f5
julio686 <julio@mbdesignremodel.com>
2026-03-21 16:15:39 -0400

    Update: Fix logo and add AI Ghost Agent system

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -53,7 +53,7 @@ const Footer = () => {
           <div className="lg:col-span-2">
             <a href="#" className="inline-block mb-6 group">
               <img 
-                src="./logo.jpg" 
+                src="logo.jpg" 
                 alt="Strivana" 
                 className="h-12 w-auto brightness-0 invert transition-transform duration-300 group-hover:scale-105"
               />
